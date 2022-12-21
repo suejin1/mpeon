ee29e20173d0b41e07e569d3589c47bd741f7f92
Hyunjin Choi <chg5638@gmail.com>
2022-12-21 23:16:05 +0000

    asdf

--- chj/cmake_test/lib/shape.cc
+++ chj/cmake_test/lib/shape.cc
@@ -7,7 +7,7 @@ Rectangle::Rectangle(int width, int height) : width_(width), height_(height) {}
 
 int Rectangle::GetSize() const {
 
-    std::thread t([this]() { std::cout << "Calulate .." << std::endl; });
+    std::thread t([this]() { std::cout << "Calculate .." << std::endl; });
     t.join();
 
     // 직사각형의 넓이를 리턴한다.
