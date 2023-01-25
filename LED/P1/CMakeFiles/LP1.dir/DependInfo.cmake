1db8f095e2fcf33d01198b9e474c8b4b95c15fc8
suejin1 <suejin@mpeon.com>
2023-01-25 10:21:17 +0900

    LED

---
+++ LED/P1/CMakeFiles/LP1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/psj/psj_docker/LED/P1/led1_p1.c" "/home/psj/psj_docker/LED/P1/CMakeFiles/LP1.dir/led1_p1.c.o"
+  "/home/psj/psj_docker/LED/P1/led2_p1.c" "/home/psj/psj_docker/LED/P1/CMakeFiles/LP1.dir/led2_p1.c.o"
+  "/home/psj/psj_docker/LED/P1/led3_p1.c" "/home/psj/psj_docker/LED/P1/CMakeFiles/LP1.dir/led3_p1.c.o"
+  "/home/psj/psj_docker/LED/P1/led4_p1.c" "/home/psj/psj_docker/LED/P1/CMakeFiles/LP1.dir/led4_p1.c.o"
+  "/home/psj/psj_docker/LED/P1/led5_p1.c" "/home/psj/psj_docker/LED/P1/CMakeFiles/LP1.dir/led5_p1.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ LED/P1/CMakeFiles/LP1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LP1.dir/led1_p1.c.o"
+  "CMakeFiles/LP1.dir/led2_p1.c.o"
+  "CMakeFiles/LP1.dir/led3_p1.c.o"
+  "CMakeFiles/LP1.dir/led4_p1.c.o"
+  "CMakeFiles/LP1.dir/led5_p1.c.o"
+  "LP1.pdb"
+  "LP1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
