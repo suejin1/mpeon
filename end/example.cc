df04d982702def7968f5b63e41710a76eb4013ef
suejin1 <121079690+suejin1@users.noreply.github.com>
2023-02-16 14:23:29 +0900

    Update example.cc

--- end/example.cc
+++ end/example.cc
@@ -9,7 +9,7 @@ void exmaple()
     data.t = localtime(&data.timer);
 
     printf("┌──────────────────────────────────────────────────────┐\n│");
-    printf("현재 시간 : %d시 %d분 %d초                             │\n│", data.t->tm_hour, data.t->tm_min, data.t->tm_sec);
+    printf("현재 시간 : %d시 %d분 %d초                            │\n│", data.t->tm_hour, data.t->tm_min, data.t->tm_sec);
 
     printf("모든 시간은 1초 단위입니다.                           │\n│");
     printf("LED :  1  LedNum  StartTime  EndTime     patter       │\n│");
@@ -22,7 +22,7 @@ void exmaple()
     printf("     ( 3    0       free      free        free      ) │\n│");
     printf("                                                      │\n│");
 
-    printf("띄어쓰기 없이 입력해주세요.                           │\n|");
-    printf("10초 내에 입력되지 않을 경우 종료됩니다.              │\n");
+    printf("띄어쓰기 없이 입력해주세요.                           │\n");
+    printf("│10초 내에 입력되지 않을 경우 종료됩니다.              │\n");
     printf("└──────────────────────────────────────────────────────┘\n");
 }
