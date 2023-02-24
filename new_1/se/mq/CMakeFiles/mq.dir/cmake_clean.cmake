fe2daee123693971c2848d0752ccaa25fd978c2a
suejin1 <121079690+suejin1@users.noreply.github.com>
2023-02-24 16:09:30 +0900

    Add files via upload

---
+++ new_1/se/mq/CMakeFiles/mq.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mq.dir/checkDate.cc.o"
+  "CMakeFiles/mq.dir/receive.cc.o"
+  "CMakeFiles/mq.dir/send.cc.o"
+  "libmq.a"
+  "libmq.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
