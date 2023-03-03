70c69a7697ca78dd2d11e1ddb955699d8048214d
suejin1 <121079690+suejin1@users.noreply.github.com>
2023-03-03 09:17:48 +0900

    Update time2.c

--- time2/time2.c
+++ time2/time2.c
@@ -9,15 +9,15 @@ int main() {
     t = localtime(&timer); // 포맷팅을 위해 구조체에 넣기
     
     printf("유닉스 타임 (Unix Time): %lld 초\n\n", timer); 
-    // printf("현재 년: %d\n", t->tm_year + 1900);
-    // printf("현재 월: %d\n", t->tm_mon + 1);
-    // printf("현재 일: %d\n", t->tm_mday);
+    printf("현재 년: %d\n", t->tm_year + 1900);
+    printf("현재 월: %d\n", t->tm_mon + 1);
+    printf("현재 일: %d\n", t->tm_mday);
     printf("현재 시: %d\n", t->tm_hour);
     printf("현재 분: %d\n", t->tm_min);
     printf("현재 초: %d\n", t->tm_sec);
-    // printf("현재 요일: %d\n", t->tm_wday); // 일=0, 월=1, 화=2, 수=3, 목=4, 금=5, 토=6
-    // printf("올해 몇 번째 날: %d\n", t->tm_yday); // 1월 1일은 0, 1월 2일은 1
-    // printf("서머타임 적용 여부: %d\n", t->tm_isdst); // 실시 중이면 양수, 미실시면 0, 실시 정보가 없으면 음수
+    printf("현재 요일: %d\n", t->tm_wday); // 일=0, 월=1, 화=2, 수=3, 목=4, 금=5, 토=6
+    printf("올해 몇 번째 날: %d\n", t->tm_yday); // 1월 1일은 0, 1월 2일은 1
+    printf("서머타임 적용 여부: %d\n", t->tm_isdst); // 실시 중이면 양수, 미실시면 0, 실시 정보가 없으면 음수
     
     return 0;
 }
